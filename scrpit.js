ea520897c0aac1b26c26c7eea913340ec434c39f
Piranavan <piranavanpiran2000@gmail.com>
2023-11-11 23:59:27 +0530

    added3

---
+++ scrpit.js
@@ -0,0 +1,42 @@
+
+// Dark mode
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+
+  // Get the image element by its ID
+  var image = document.getElementById('modeIcon');
+
+  // Get the current source of the image
+  var currentSrc = image.src;
+
+  // Check the current source and change it based on the mode
+  if (currentSrc.includes('moon(dark)')) {
+      // If it's in dark mode, switch to light mode image
+      image.src = './Assets/img/sun(dark).svg';
+  } else {
+      // If it's in light mode, switch to dark mode image
+      image.src = './Assets/img/moon(dark).svg';
+  }
+}
+
+
+//  fQAs
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+
